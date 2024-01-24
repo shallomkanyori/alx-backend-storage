3ae436b365c873199797e009abb403d1b5b3e394
Shallom Kanyori <shallomkanyori@gmail.com>
2024-01-24 09:14:27 +0000

    Add a method to read from Redis to the Cache class
    Add a method that reads from Redis with the option of recovering the
    original type to the Cache class.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,10 @@ Classes:
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, TypeVar
+
+
+T = TypeVar('T', str, bytes, int, float)
 
 
 class Cache():
@@ -18,10 +21,32 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: T) -> str:
         """Stores data in Redis with a random key and returns the key."""
+
         key = str(uuid.uuid4())
 
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn: Callable[[bytes], T] = None) -> Union[T, None]:
+        """Returns the value associated with `key`.
+        Optionally converts it back to the desired format using `fn`.
+        """
+
+        data = self._redis.get(key)
+        if data and fn:
+            return fn(data)
+        else:
+            return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """Returns the string value at `key`"""
+
+        return self.get(key, lambda x: str(x))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """Returns the int value at `key`"""
+
+        return self.get(key, lambda x: int(x))
